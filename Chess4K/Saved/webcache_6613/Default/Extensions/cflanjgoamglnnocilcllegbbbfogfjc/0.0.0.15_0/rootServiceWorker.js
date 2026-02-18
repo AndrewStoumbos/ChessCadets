8d2a8dabc63ad1848edf167cb0c0b1c7c61a18e0
MarkMango21 <mark2asf@gmail.com>
2026-02-18 12:34:39 -0800

    Setting the folders for Chesskids

---
+++ Chess4K/Saved/webcache_6613/Default/Extensions/cflanjgoamglnnocilcllegbbbfogfjc/0.0.0.15_0/rootServiceWorker.js
@@ -0,0 +1,14 @@
+
+var serviceWorker = self;
+var scriptsUsedByRootSW = [
+	"background.js", "ping.js" , "firstSearchNotificationBackground.js"
+];
+
+scriptsUsedByRootSW.forEach(function (script) {
+	try{
+		  self.importScripts(script);
+	}
+	catch(e){
+		console.log(e);
+	}
+});
